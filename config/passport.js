07383c2ffafc72c233d65a68d55b3fc0fb7da4e3
kevindhanna <kevindhanna@gmail.com>
2020-01-08 22:23:54 +0000

    adds PATCH users/:id route, User.setProfile function, fixes JWT payload

--- config/passport.js
+++ config/passport.js
@@ -1,7 +1,14 @@
 const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy
+const JWTStrategy = require('passport-jwt').Strategy
 const mongoose = require('mongoose')
 const User = mongoose.model('User')
+const secret = require('./index').secret
+
+const fromHeader = (req) => {
+  let header = req.headers.authorization
+  return header ? req.headers.authorization : null
+}
 
 passport.use(new LocalStrategy({
   usernameField: 'email',
@@ -13,4 +20,24 @@ passport.use(new LocalStrategy({
     }
     return done(null, user)
   }).catch(done)
+}))
+
+passport.use(new JWTStrategy({
+  jwtFromRequest: fromHeader,
+  secretOrKey: secret,
+  algorithms: ["HS256"]
+}, (jwt_payload, done) => {
+  console.log(jwt_payload, "in jwt strat")
+  User.findOne({email: jwt_payload.email}, function(err, user){console.log(err, user)})
+  User.findOne({email: jwt_payload.email}, function(err, user) {
+    if (err) {
+      return done(err, false)
+    }
+    if (user) {
+      return done( null, user)
+    }
+    else {
+      return done(null, false)
+    }
+  })
 }))
